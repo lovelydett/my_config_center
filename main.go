7ab82cbeb465c080e0cc2bc1f360f0dca47fea67
lovelydett <xyt@bupt.cn>
2024-07-22 17:58:54 +0800

    Refine the static file hanlder and enable the website icon

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	router := initAppRouter()
 	http.Handle("/", router)
-	log.Println("Server started at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server started at http://localhost:8889")
+	if err := http.ListenAndServe(":8889", nil); err != nil {
+		log.Fatal(err)
+	}
 }

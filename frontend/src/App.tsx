f2496da0daa614342e009d32a9004b7db794dad4
Yuting Xie <xytxytxyt@hotmail.com>
2024-11-11 00:07:47 -0500

    Refactor frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// P for props, S for state, SS for snapshot
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Welcome to Wolf! </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
